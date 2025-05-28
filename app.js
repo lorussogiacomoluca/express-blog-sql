9fed05a088f82e8ccfb9aaabf8f043dfb8fa1a2d
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-28 15:00:07 +0200

    [UPD] Setting post router and test /post get request

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const connection = require("./data/db");
 
+//Routers
+const postRouter = require("./routers/post.js");
+
 //Express
 const express = require("express");
 const app = express();
@@ -9,6 +12,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to my blog");
   console.log("Welcome to my blog");
 });
+
+app.use("/post", postRouter);
 
 app.listen(port, () => {
   console.log(`On listening on port ${port}`);
