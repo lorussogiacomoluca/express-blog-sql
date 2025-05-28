f6fb7ee533a68996ee2d54c35e1e6abd71491ad1
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-28 14:45:24 +0200

    [UPD] Express configuration in app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const connection = require("./data/db");
+
+//Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my blog");
+  console.log("Welcome to my blog");
+});
+
+app.listen(port, () => {
+  console.log(`On listening on port ${port}`);
+});
