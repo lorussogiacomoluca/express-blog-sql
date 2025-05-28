d9b5e8cec0dbc74c0cfd90e72ecb2bb00d079ed7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-05-28 15:02:42 +0200

    [UPD] Index controller for post

---
+++ controllers/postController.js
@@ -0,0 +1,13 @@
+const connection = require("../data/db");
+
+const index = (req, res) => {
+  const sql = `SELECT * FROM posts`;
+  connection.query(sql, (error, resutls) => {
+    if (error) {
+      return res.status(500).json({ message: error.message });
+    }
+    res.send(resutls);
+  });
+};
+
+module.exports = { index };
